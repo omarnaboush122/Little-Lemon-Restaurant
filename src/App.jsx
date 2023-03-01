8c5bd319d6f9917497c5a0450db031071e5dbbed
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-01 14:56:39 +0200

    add navbar icons hamburger menu and close menu and a state to toggle between them

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 
 const App = () => {
   return (
     <>
-    <Header/>
+    <Navbar/>
     <Main/>
     <Footer/>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,59 @@
-import React from 'react';
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isopenMenu, setIsOpenMenu] = useState(false);
+
+  const openMenu = () => {
+    setIsOpenMenu(true);
+  };
+
+  const closeMenu = () => {
+    setIsOpenMenu(false);
+  };
   return (
-    <nav>
-      <ul>
-        <li><a href="#Home">Home</a></li>
-        <li><a href="#About">About</a></li>
-        <li><a href="#Menu">Menu</a></li>
-        <li><a href="#Reservations">Reservations</a></li>
-        <li><a href="#Order Online">Order Online</a></li>
-        <li><a href="#">Login</a></li>
+    <nav className="mx-6 mt-6">
+      <div className="flex w-full items-center justify-between">
+        <img src="./assets/Logo.svg" alt="little lemon logo" />
+        <button className="cursor-pointer">
+          {isopenMenu ? (
+            <img
+              src="./assets/close-icon.png"
+              alt="close-icon"
+              onClick={closeMenu}
+              className="w-10 h-10"
+            />
+          ) : (
+            <img
+              src="./assets/🦆 icon _hamburger menu.svg"
+              alt="hamburger menu"
+              onClick={openMenu}
+              className="w-10 h-10"
+            />
+          )}
+        </button>
+      </div>
+      <ul className="hidden">
+        <li>
+          <a href="#Home">Home</a>
+        </li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#Menu">Menu</a>
+        </li>
+        <li>
+          <a href="#Reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="#Order Online">Order Online</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
