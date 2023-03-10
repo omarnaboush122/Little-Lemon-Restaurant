92ab58e77f788942e15c50dd7707aaf94f280504
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-10 18:56:19 +0200

    change confirmation to components

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Form from "../components/reservations/Form";
 import ReservationMain from "../components/reservations/ReservationMain";
-import Confirmation from "../confirmation/Confirmation";
+import Confirmation from "../components/confirmation/Confirmation";
 
 const Reservations = () => {
   const [isReserved, setIsReserved] = useState(false);
